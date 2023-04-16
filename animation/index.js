cdf677dce1d6f13f2f4c47aa916a6b0d0f73ebb7
Hasan Moni <62002387+hasan-moni-321@users.noreply.github.com>
2023-04-16 12:42:22 +0600

    Add files via upload

---
+++ animation/index.js
@@ -0,0 +1,25 @@
+$("#btn").click(function(){
+    // $('#div1').show(2000); 
+    // $("#div1").show(2000); 
+    // $('#div1').hide(2000).show(2000); 
+    // $('#div1').toggle(2000) // show hide together
+    // $('#div1').fadeOut(2000)
+    // $('#div1').fadeIn(2000)
+    // $('#div1').fadeOut(2000).fadeIn(2000); 
+    // $('#div1').fadeToggle(2000) // fadein fadeout together
+    // $('#div1').fadeTo(2000, 0.4) 
+    // $('#div1').slideUp(2000)
+    // $('#div1').slideDown(2000)
+    // $('#div1').slideUp(2000).slideDown(2000); 
+    // $('#div1').slideToggle(2000) 
+
+
+    // $('#div1').animate({opacity: "0.5"}, 2000)
+    $('#div1').animate({
+        height: "500px", 
+        width: "900px",
+        marginLeft: "30px"
+    }, 2000)
+
+})
+
