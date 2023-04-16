e595f4afcb836a2527fec035cb729e6bdae124ed
Hasan Moni <62002387+hasan-moni-321@users.noreply.github.com>
2023-04-16 12:41:26 +0600

    Add files via upload

---
+++ css_style_manipulating/index.js
@@ -0,0 +1,23 @@
+// using jquery 
+// $('h1').css("color", "red"); 
+// $('h1').css('font-size', '3rem'); 
+// $('h1').css('font-style', 'italic'); 
+
+// using jquery with json
+/*
+$('h1').css({
+    'color': 'green',
+    'font-size': '5rem', 
+    'font-style': 'italic'
+})
+*/ 
+
+
+// using javascript with external style.css file 
+// document.querySelector('h1').classList.add('style1')
+
+// Jquery 
+$('h1').addClass('style1')
+
+// adding multiple class 
+$('h1').addClass('style1, style2'); 
