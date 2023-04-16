7bdcea95f1eae829b8740cccc425aae5883072e0
Hasan Moni <62002387+hasan-moni-321@users.noreply.github.com>
2023-04-16 12:42:01 +0600

    Add files via upload

---
+++ attrubute_manipulation/index.js
@@ -0,0 +1,14 @@
+// using javascript 
+// var myattr = document.querySelector('a').getAttribute('href'); 
+// console.log(myattr); 
+
+// using jqury
+// var jq_attr = $("a").attr('href')
+// console.log(jq_attr); 
+
+
+// set attribute using javascript
+// document.querySelector('a').setAttribute('href', "http://www.studywithanis.com");
+
+// set attribute using jquery 
+$('a').attr("href", "https://www.studywithanis.com"); 
